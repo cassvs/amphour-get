74fdb68655e564286519e9c8141b2f5d3b842d47
Cass S <cassvsmith@gmail.com>
2016-09-05 23:49:48 -0400

    Update ahget.pl

--- ahget.pl
+++ ahget.pl
@@ -10,25 +10,24 @@ my $usage = "Usage: $0 episode_number [another_episode_number ...]\n";
 
 die "$usage" unless @ARGV; #Print usage if there are no arguments.
 
-print "Fetching index...   ";
+#Make sure each argument is a number, and push each one into an array.
+foreach my $arg (@ARGV) {
+	$arg =~ m/\d+\z/ or die "Argument $arg is not a number!\n$usage";
+	push(@args, $arg);
+}
+@args = uniq(@args); #Remove duplicate elements from the array.
+
+#Make sure the user knows what they're doing. :)
+print "Warning: going to download " . scalar(@args) . " files, which could be 30-40MB each.\nAre you sure? [y/N]:";
+chomp ($_ = <STDIN>);
+die "Ok, aborting.\n" unless (/^[Yy](?:es)?$/);	
+
+print "Ok, fetching index...   ";
 my $index = `curl -sS $indexurl`; #Use cURL to fetch index webpage.
 print "Fetched!\n";
 
 #Break the index page into chunks. There *should* be one usable link per chunk.
 my @indexarray = split /Direct download:/, $index;
-
-#Make sure each argument is a number, and push them into an array.
-foreach my $arg (@ARGV) {
-	$arg =~ m/\d+\z/ or die "Argument $arg is not a number!\n$usage";
-	push(@args, $arg);
-}
-
-@args = uniq(@args); #Remove duplicate elements from the array.
-
-#Make sure the user knows what they're doing. :)
-print "Warning: going to download " . scalar(@args) . " files, which could be 30-40MB each. Are you sure? [y/N]:";
-chomp ($_ = <STDIN>);
-die "Ok, aborting.\n" unless (/^[Yy](?:es)?$/);	
 
 #Search the index chunks for the URLs of the specified episodes.
 OUTER:	foreach my $number (@args) {
